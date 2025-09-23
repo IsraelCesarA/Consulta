8631449bf38892ce6ae1be578a4f8418810a6937
Israel César <113929138+IsraelCesarA@users.noreply.github.com>
2025-09-23 10:21:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -264,9 +264,13 @@ function escrever_tabela(json_linhas_selecionadas){
     let th = thead.insertRow();
     
     let cabecalhos = ['Linha', 'Emp', 'Tab', 'Posto', 'Início', 'Carro', 'Horário Real'];
-    cabecalhos.forEach(texto => {
+    cabecalhos.forEach((texto, index) => {
         let celula = th.insertCell();
         celula.innerText = texto;
+        // Se a célula for a de "Posto", adicione a classe para escondê-la
+        if (texto === 'Posto') {
+            celula.classList.add('hide-posto');
+        }
     });
 
     // Aplicando a classe para fixar o cabeçalho
@@ -297,7 +301,11 @@ function escrever_tabela(json_linhas_selecionadas){
         tr.insertCell().innerText = item.linha;
         tr.insertCell().innerText = item.empresa;
         tr.insertCell().innerText = item.tabela;
-        tr.insertCell().innerText = item.postoControle;
+        
+        // Célula do posto: adicione o valor e a classe para esconder
+        let postoCell = tr.insertCell();
+        postoCell.innerText = item.postoControle;
+        postoCell.classList.add('hide-posto');
         
         const scheduledTime = new Date(agora.getFullYear(), agora.getMonth(), agora.getDate(), item.horario.split(':')[0], item.horario.split(':')[1], 0);
         
